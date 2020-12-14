2921e8eadf3f2ea79a639fb50a3f42e035cdb265
Ophir Taieb <ophirtaieb1234@gmail.com>
2020-12-14 21:35:17 +0200

    First MVP - Moving each robot to toward its destination without trying to resolve conflicts with other robots

--- src/navie_solver.py
+++ src/navie_solver.py
@@ -1,67 +1,75 @@
-import networkx as nx
-
-from resources.teacher_example_solver import read_scene
-from src.robot import Robot
+import utils
 
 
-def update_graph(grid):
-    """
-    need to update the graph after we make a moves
-    """
-    return None
-
-
-def move_robot(robot, invalid_positions):
+def calc_robot_next_step(robot, invalid_positions):
     """
     need to calculate shortest path +
     update robot current pos +
     add move to data structure
     """
-    return None
+    go_right = (robot.target_pos[0] - robot.current_pos[0]) > 0
+    go_up = (robot.target_pos[1] - robot.current_pos[1]) > 0
+    go_left = (robot.target_pos[0] - robot.current_pos[0]) < 0
+    go_down = (robot.target_pos[1] - robot.current_pos[1]) < 0
+    if go_right:
+        right_pos = utils.calc_next_pos(robot.current_pos, 'right')
+        if right_pos not in invalid_positions:
+            return right_pos, utils.STEP_DIRECTION['right']
+    if go_left:
+        left_pos = utils.calc_next_pos(robot.current_pos, 'left')
+        if left_pos not in invalid_positions:
+            return left_pos, utils.STEP_DIRECTION['left']
+    if go_up:
+        up_pos = utils.calc_next_pos(robot.current_pos, 'up')
+        if up_pos not in invalid_positions:
+            return up_pos, utils.STEP_DIRECTION['up']
+    if go_down:
+        down_pos = utils.calc_next_pos(robot.current_pos, 'down')
+        if down_pos not in invalid_positions:
+            return down_pos, utils.STEP_DIRECTION['down']
+    return robot.current_pos, None
 
 
 def sort_robots(robots):
     return sorted(robots, key=lambda robot: robot.distance)
 
 
-def calculate_distance(robots):
+def update_robots_distances(robots):
     for robot in robots:
         target_pos = robot.target_pos
-        start_pos = robot.start_pos
-        robot.distance = (int)(abs(target_pos[0] - start_pos[0]) + abs(target_pos[1] - start_pos[1]))
+        current_pos = robot.current_pos
+        robot.distance = abs(target_pos[0] - current_pos[0]) + abs(target_pos[1] - current_pos[1])
+
+
+def move_robot(robot, robot_next_pos, invalid_positions):
+    invalid_positions.remove(robot.current_pos)
+    robot.current_pos = robot_next_pos
+    invalid_positions.add(robot.current_pos)
+
+
+def is_not_finished(robots):
+    return all(robot.current_pos == robot.target_pos for robot in robots)
 
 
 def solve(infile: str, outfile: str):
-    scene, name = read_scene.read_scene(infile)
-    robots_num = len(scene[0])
-    robots = []  # list of robots to hold data for each robot in the grid.
-    obstacles = set(tuple(a) for a in scene[2])
-    for robot_index in range(robots_num):
-        robots.append(Robot(robot_index, tuple(scene[0][robot_index]), tuple(scene[1][robot_index])))
+    robots, obstacles, name = utils.read_scene(infile)
+    invalid_positions = set(obstacles).union(set(robot.current_pos for robot in robots))
 
-    invalid_positions = set(obstacle for obstacle in obstacles)
-    invalid_positions.add(robot.current_pos for robot in robots)
-
-    calculate_distance(robots)
+    update_robots_distances(robots)
     robots = sort_robots(robots)
-    # Calculate scene bounding box
-    all_points = [robot.start_pos for robot in robots] + [robot.target_pos for robot in robots] + list(obstacles)
-    min_x = min(a[0] for a in all_points)
-    max_x = max(a[0] for a in all_points)
-    min_y = min(a[1] for a in all_points)
-    max_y = max(a[1] for a in all_points)
-
-    grid = [(x, y) for x in range(min_x - 1, max_x + 2) for y in range(min_y - 1, max_y + 2)]
-    G = nx.Graph()
-
-    flag = True
-    moves = []  # a data structure to hold all the moves for each robot
-    while flag:  # while not all robots finished
-        for prior_index in range(len(robots)):  # move each robot accordingly to its priority
-            move_robot(robots[prior_index], invalid_positions)
-            update_graph(grid)
+    steps = []  # a data structure to hold all the moves for each robot
+    step_number = 0
+    while not is_not_finished(robots):  # while not all robots finished
+        steps.append(dict())
+        for robot in robots:  # move each robot accordingly to its priority
+            robot_next_pos, robot_step = calc_robot_next_step(robot, invalid_positions)
+            move_robot(robot, robot_next_pos, invalid_positions)
+            if robot_step:
+                steps[step_number][str(robot.index)] = robot_step
+        step_number += 1
 
     # after the algorithm finished, we should write the moves data structure to json file.
+    utils.write_solution(steps, name, outfile)
 
 
 if __name__ == "__main__":
---
+++ src/utils.py
@@ -0,0 +1,25 @@
+import json
+
+from robot import Robot
+
+
+def read_scene(scene_path: str):
+    with open(scene_path, 'r') as f:
+        scene = json.load(f)
+    starts, targets, obstacles = scene['starts'], scene['targets'], scene['obstacles']
+    robots = [Robot(index, tuple(starts[index]), tuple(targets[index])) for index in range(len(starts))]
+    obstacles = set(tuple(a) for a in obstacles)
+    return robots, obstacles, scene['name']
+
+
+DIRECTION_TO_VECTOR = {'up': (0, 1), 'right': (1, 0), 'down': (0, -1), 'left': (-1, 0), 'halt': (0, 0)}
+STEP_DIRECTION = {'up': 'N', 'right': 'E', 'down': 'S', 'left': 'W'}
+
+
+def calc_next_pos(location, direction):
+    return location[0] + DIRECTION_TO_VECTOR[direction][0], location[1] + DIRECTION_TO_VECTOR[direction][1]
+
+
+def write_solution(steps, name, outfile):
+    with open(outfile, 'w') as f:
+        f.write(json.dumps({"instance": name, "steps": steps}, indent=4))
