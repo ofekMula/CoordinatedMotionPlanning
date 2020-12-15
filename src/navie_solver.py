70600aad90b584571de4eb1bb032824f889cf489
Ofek Mula <ofek1210@gmail.com>
2020-12-15 15:38:32 +0200

    - distance calculation with graph.

--- src/navie_solver.py
+++ src/navie_solver.py
@@ -1,3 +1,4 @@
+import networkx as nx
 import utils
 import os
 import time
@@ -44,12 +45,37 @@ def sort_robots(robots):
     return sorted(robots, key=lambda robot: robot.distance)
 
 
-def update_robots_distances(robots):
+def update_robots_distances(robots, graph):
     for robot in robots:
         target_pos = robot.target_pos
         current_pos = robot.current_pos
-        robot.distance = abs(target_pos[0] - current_pos[0]) + abs(target_pos[1] - current_pos[1])
-
+        #robot.distance = abs(target_pos[0] - current_pos[0]) + abs(target_pos[1] - current_pos[1])
+        if nx.has_path(graph, current_pos, target_pos):
+            sp = nx.shortest_path(graph, current_pos, target_pos)
+            robot.distance = len(sp)-1
+def create_graph(robots, obstacles):
+    all_points = [robot.current_pos for robot in robots] + [robot.target_pos for robot in robots] + list(obstacles)
+    min_x = min(a[0] for a in all_points)
+    max_x = max(a[0] for a in all_points)
+    min_y = min(a[1] for a in all_points)
+    max_y = max(a[1] for a in all_points)
+    grid = [(x, y) for x in range(min_x - 1, max_x + 2) for y in range(min_y - 1, max_y + 2)]
+    graph = nx.Graph()
+    graph.add_nodes_from(grid)
+    for u in graph.nodes:
+        # Add valid edges
+        for x in range(max(min_x - 1, u[0] - 1), min(max_x + 1, u[0] + 1)):
+            if x == u[0]:
+                for y in range(max(min_y - 1, u[1] - 1), min(max_y + 1, u[1] + 1)):
+                    v = (x, y)
+                    if v not in obstacles:
+                        graph.add_edge(u, v)
+            else:
+                y = u[1]
+                v = (x, y)
+                if v not in obstacles:
+                    graph.add_edge(u, v)
+    return graph
 
 def move_robot(robot, robot_next_pos, invalid_positions):
     invalid_positions.remove(robot.current_pos)
@@ -74,13 +100,13 @@ def solve(infile: str, outfile: str):
     global log
     log = Logger(os.path.split(file_name)[1])
     invalid_positions = set(obstacles).union(set(robot.current_pos for robot in robots))
-
-    update_robots_distances(robots)
+    graph = create_graph(robots, obstacles)
+    update_robots_distances(robots, graph)
     robots = sort_robots(robots)
     steps = []  # a data structure to hold all the moves for each robot
     step_number = 0
     while is_not_finished(robots) and is_not_stuck(steps):  # while not all robots finished
-        steps.append(dict())
+        steps.append(dict())  # each step holds dictionary <robot_index,robot_step>
         for robot in robots:  # move each robot accordingly to its priority
             robot_next_pos, robot_step = calc_robot_next_step(robot, invalid_positions)
             move_robot(robot, robot_next_pos, invalid_positions)
@@ -98,5 +124,7 @@ def solve(infile: str, outfile: str):
 
 
 if __name__ == "__main__":
-    for file_name in os.listdir('../tests/inputs/'):
-        solve(infile=f'../tests/inputs/{file_name}', outfile=f'../tests/outputs/{file_name}')
+    # for file_name in os.listdir('../tests/inputs/'):
+    #     solve(infile=f'../tests/inputs/{file_name}', outfile=f'../tests/outputs/{file_name}')
+    file_name = 'scene_2.json'
+    solve(infile=f'../tests/inputs/{file_name}', outfile=f'../tests/outputs/{file_name}')
